42cc606c6c091dbf4c30e0e21d494494c04d599d
nneversky <skeleton.ubica@gmail.com>
2024-08-18 21:23:36 +0300

    ref index/utils

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import pageLoader from '../src/index.js';
-import debug from 'debug';
-
-const log = debug('page-loader');
 
 const program = new Command();
 program
@@ -14,7 +11,8 @@ program
 
   .action((url) => {
     const options = program.opts();
-    log(`Start main function "pageLoader" on url: ${url}`);
-    pageLoader(url, options);
+    pageLoader(url, options)
+      .then((filepath) => console.log(`Page was successfully downloaded into '${filepath}'\n`))
+      .catch((err) => console.log(err));
   });
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -9,9 +9,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default (url, options) => {
-  const hrefURL = new URL(url);
-  const nameHtmlFile = `${hrefURL.host.replaceAll('.', '-')}${hrefURL.pathname.replaceAll('/', '-')}.html`;
-  const filePath = (options.output || path.join(__dirname, '..', '/user/current-dir')) + '\\' + nameHtmlFile;
-  log(`Create filepath: ${filePath}`);
-  downloadPage(url, filePath);
+  return new Promise((resolve, reject) => {
+    try {
+      const hrefURL = new URL(url);
+      const nameHtmlFile = `${hrefURL.host.replaceAll('.', '-')}${hrefURL.pathname.replaceAll('/', '-')}.html`;
+      const filePath = (options.output || path.join(__dirname, '..', '/user/current-dir')) + '\\' + nameHtmlFile;
+      log(`Create filepath: ${filePath}`);
+      downloadPage(url, filePath);
+      resolve(filePath);
+    } catch (err) {
+      reject(err);
+    }
+  });
 };
