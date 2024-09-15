deaee736c2a3ad419865b9cf277ff4206a67d52e
nneversky <skeleton.ubica@gmail.com>
2024-09-15 23:02:43 +0300

    ref index.test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,9 +2,12 @@ import nock from 'nock';
 import path from 'path';
 import os from 'os';
 import downloadPage from '../src/utils.js';
+import { beforeEach, test, expect, describe } from '@jest/globals';
 
 const tmpFilePath = (nameDir) => path.join(os.tmpdir(), nameDir);
 
-test('test', async () => {
-  await downloadPage('https://ru.hexlet.io/courses', tmpFilePath('tmpDir\\ru-hexlet-io-courses.html'));
+describe('Successful download', () => {
+  test('Download page', async () => {
+    await downloadPage('https://ru.hexlet.io/courses', tmpFilePath('tmpDir\\ru-hexlet-io-courses.html'));
+  });
 });
