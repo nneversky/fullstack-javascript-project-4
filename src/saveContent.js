e380b1ea1b6571973cdaa4a38d8d3b7f32bec25f
nneversky <skeleton.ubica@gmail.com>
2024-10-16 12:16:36 +0300

    ref import cheerico

--- src/saveContent.js
+++ src/saveContent.js
@@ -1,5 +1,5 @@
 import fsp from 'fs/promises';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import fetch from 'node-fetch';
 import path from 'path';
 import debug from 'debug';
