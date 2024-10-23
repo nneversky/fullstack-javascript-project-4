ccad1208644fc029b3671418198506bcad5bdf1c
nneversky <skeleton.ubica@gmail.com>
2024-10-23 12:40:25 +0300

    add validUrl

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,21 +1,53 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import axios from 'axios';
 import pageLoader from '../src/index.js';
 
 const program = new Command();
+
+const isValidUrl = (url) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+const checkUrlExists = async (url) => {
+  try {
+    await axios.get(url);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
 program
   .version('0.0.1')
   .arguments('<url>')
   .description('Page loader utility')
   .option('-o, --output [dir]', 'output dir (default: "/home/user/current-dir")')
-
-  .action((url) => {
+  .action(async (url) => {
     const options = program.opts();
+
+    if (!isValidUrl(url)) {
+      console.error('Invalid URL. Please provide a valid URL.');
+      process.exit(1);
+    }
+
+    const urlExists = await checkUrlExists(url);
+    if (!urlExists) {
+      console.error('URL does not exist or cannot be reached.');
+      process.exit(1);
+    }
+
     pageLoader(url, options)
       .then((filepath) => console.log(`Page was successfully downloaded into '${filepath}'\n`))
       .catch((err) => {
-        console.log(err)
-        process.exit(1)
+        console.error(err);
+        process.exit(1);
       });
   });
+
 program.parse(process.argv);
